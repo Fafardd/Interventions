acfecd4c12e447cb00bd6409e432187f0026ae4b
unknown <kevin.fafard1@hotmail.com>
2018-03-27 15:53:13 -0400

    Ajout de validator avec une longueur minimal, tp9

--- src/app/shared/caracteres-validator.spec.ts
+++ src/app/shared/caracteres-validator.spec.ts
@@ -17,16 +17,44 @@ describe('sansEspaces Validator',()=>{
     });
 
     it('une chaine avec des mots', ()=>{
-        let control = { value: 'kevin' };
+        let control = { value: 'kevin est un etudiant' };
         let validator = VerifierEspaceValidator.sansEspaces();
         let result = validator(control as AbstractControl);
         expect(result['sansEspaces']).toBe(true);
     });
 
-    it('une chaine avec des mots', ()=>{
+    it('une phrase avec des espaces, un mot et des espaces', ()=>{
         let control = { value: '   qwe   ' };
         let validator = VerifierEspaceValidator.sansEspaces();
         let result = validator(control as AbstractControl);
         expect(result['sansEspaces']).toBe(true);
+    });
+
+    it('une expression avec 1 espace et 2 caractère est invalide', ()=>{
+        let control = { value: ' xx'};
+        let validator = VerifierEspaceValidator.longueurMinimum(3);
+        let result = validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(false);
+    });
+
+    it('une expression avec 2 espaces et 1 caractère est invalide', ()=>{
+        let control = { value: '  x'};
+        let validator = VerifierEspaceValidator.longueurMinimum(3);
+        let result = validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(false);
+    });
+
+    it('une phrase avec 3 espaces et 3 caractères est valide', ()=>{
+        let control = { value: "   J'aime Angular"};
+        let validator = VerifierEspaceValidator.longueurMinimum(3);
+        let result = validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(true);
+    });
+
+    it('une phrase avec 5 espaces, 5 caractères et 5 espaces est valide', ()=>{
+        let control = { value: "     J'aime Angular     "};
+        let validator = VerifierEspaceValidator.longueurMinimum(3);
+        let result = validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(true);
     });
 });
--- src/app/shared/caracteres-validator.ts
+++ src/app/shared/caracteres-validator.ts
@@ -9,4 +9,14 @@ export class VerifierEspaceValidator{
             return{'sansEspaces':true};
         };
     }
+
+    static longueurMinimum(min: number): ValidatorFn {
+        return(c: AbstractControl): { [key: string]: boolean} | null =>{
+            if(c.value.trim().length >= min){
+                return{'longueurMinimum' : true}
+            }
+
+            return{'longueurMinimum':false};
+        };
+    }
 }
